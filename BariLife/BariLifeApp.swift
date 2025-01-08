683624545f4a7d5ff25904c403be829d37dd8fe5
Marco Horstmann <marco@horstmann.in>
2025-01-08 21:39:35 +0100

    Initial Commit

---
+++ BariLife/BariLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BariLifeApp.swift
+//  BariLife
+//
+//  Created by Marco Horstmann on 08.01.25.
+//
+
+import SwiftUI
+
+@main
+struct BariLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
